95dc51fff8079101408ecf79d7f31e404f0995f2
Marlon Peseke <mpeseke@gmail.com>
2018-07-26 09:44:32 -0600

    added placeholders for Anthony. Will come back and finish persona later.

--- epic/anthonyPersona.php
+++ epic/anthonyPersona.php
@@ -4,10 +4,18 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+		<style>
+			body {
+				font-family: sans-serif;
+			}
+		</style>
+
 		<title>Anthony McMillan</title>
 	</head>
 
 	<body>
+		<h1>Anthony McMillan</h1>
+			<h2>26 year old male, single, Financial Services Adviser, Income Range $50-60k/annual26 year old male, single, Financial Services Adviser, Income Range $50-60k/annual</h2>
 
 	</body>
 </html>
