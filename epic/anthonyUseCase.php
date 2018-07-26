685add38dcf3d8638e5c4fae9dc5fd3bb42ea377
Marlon Peseke <mpeseke@gmail.com>
2018-07-26 13:31:33 -0600

    Added Anthony Use Case.

---
+++ epic/anthonyUseCase.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Use Case & User Story</title>
+	</head>
+	<body>
+		<h1>Use Case</h1>
+		<p>As a newbie to the nerd scene, I would like to be able to find like minded individuals who can guide me in the<br>
+			right direction of interest while also being able to attend events I am interested and showcase my new skills</p>
+		<h1>User Story</h1>
+		<p>Anthony is looking for a quick way to meet up with a consistent group of people in the tabletop gaming community. </p>
+		<h1>Interaction Flow</h1>
+		<ul>
+			<li>Anthony logs onto the NerdNook </li>
+			<li>The NerdNook displays a home page with sections divided into interest categories.</li>
+			<li>Anthony navigates to the tabletop RPG portion of the site.  </li>
+			<li>NerdNook populates a list of “Events” created by other uses who are looking for game groups, in order from most recent to latest.</li>
+			<li>Anthony selects an “Event” that interests him, with a game that he already plays, Pathfinder. </li>
+			<li>NerdNook opens the “Event”, displayed with multiple “Comments”, created by a user with high “Reputation.” </li>
+			<li>Anthony adds to the “Comments” and begins to discuss where the group will “Check-In” to the “Event.”</li>
+		</ul>
+
+	</body>
+</html>
