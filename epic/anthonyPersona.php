c112cde9563b4e2c559cb84915aec6e097af580a
Marlon Peseke <mpeseke@gmail.com>
2018-07-26 13:25:25 -0600

    added Anthony Persona

--- epic/anthonyPersona.php
+++ epic/anthonyPersona.php
@@ -15,7 +15,53 @@
 
 	<body>
 		<h1>Anthony McMillan</h1>
-			<h2>26 year old male, single, Financial Services Adviser, Income Range $50-60k/annual26 year old male, single, Financial Services Adviser, Income Range $50-60k/annual</h2>
+		<img src="../epic/images/anthony-image.jpg" alt="image of Anthony"/>
+		<br/>
+			<p>
+			<strong>Name: Anthony McMillan </strong> <br/>
+			<strong>Gender: Male </strong> <br/>
+			<strong>Age: 26 </strong> <br/>
+			<strong>Marital Status: Single </strong> <br/>
+			<strong>Profession: Financial Services Adviser </strong> <br/>
+			<strong>Income Range:$50-60K/annually </strong> <br/>
+			</p>
+		<h1>Attitude: </h1>
+			<p>
+				Anthony is an avid table top gamer who recently moved into a new community.
+				Being a new arrival, he has no way to connect with a group to start a new campaign.
+				Having a busy life as a financial adviser, he cannot make the time to connect with others to start a new game.
+			</p>
+		<h1>Likes: </h1>
+			<ul>
+				<li> Table Top RPG's</li>
+				<li> Mobile Gaming</li>
+				<li> Hanging out with his 14 year old brother on the weekend. </li>
+				<li> Reading comic books.</li>
+			</ul>
+		<h1>Needs: </h1>
+			<ul>
+				<li> The ability to browse through local events to find if a group is already showing interest. </li>
+				<li> Mobile format for quick reference. </li>
+				<li> A way to build community despite a busy life.</li>
+				<li> A safe way to introduce his brother to the nerding community.</li>
+			</ul>
+		<h1> Frustrations: </h1>
+			<ul>
+				<li> No way to engage the community outside of going to a local store.</li>
+				<li> Having to go online to play i.e. D20, Reddit, etc.</li>
+				<li> All past attempts to touch base have been cut short by a forum based or dated site that was not mobile friendly.</li>
+			</ul>
+		<h1> Technology:</h1>
+			<h3> Comfort Level and Hardware </h3>
+				<p>
+					Anthony uses a MacBook Air with 1.8GHz dual-core Intel Core i5 processor, Turbo Boost up to 2.9GHz, 8GB 1600MHz LPDDR3 memory,
+					512GB SSD storage, Intel HD Graphics 6000 as his primary computer, mostly for business use.
+					<br/>
+					He has a Samsung Galaxy Note 8 running the latest Android build to keep up with his clients and do day-to-day tasks.
+				</p>
+
+		<a href="anthonyUseCase.php">Anthony's Use Case</a>
+		<a href="conceptualModel.php">Conceptual Model</a>
 
 	</body>
 </html>
