502002ca6a58bdbc074541d27ec8319260552f29
Chelsea David <cryan17@cnm.edu>
2018-07-26 09:17:15 -0600

    @Gkephart test

---
+++ epic/carla.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Nerd-Nook Persona</title>
+	</head>
+	<body>
+		<h1>Carla Higgins</h1>
+		<img src="images/carla.jpg" alt="Carla">
+		<br>
+		<p>
+			<strong>Name:Carla Higgins</strong>  <br>
+			<strong>Gender:Female</strong><br>
+			<strong>Age:29</strong> <br>
+			<strong>Marital Status:Single and ready to mingle</strong> <br>
+			<strong>Profession:Postal Service clerk</strong> <br>
+			<strong>Income Range:$50K - $60K</strong>
+		</p>
+
+		<h1>Attitude:</h1>
+
+		<p>Carla is a quiet homebody who is looking for some more substance in her life.<br>
+			She has several interests in the "nerd community" but is unaware how to pursue these interests<br>
+			Carla's interests include but are not limited to:</p>
+		<ul>
+			<li>Cosplay</li>
+			<li>Gaming - all forms</li>
+			<li>Book Clubs</li>
+			<li>Movie clubs</li>
+		</ul>
+		<p>With that in mind, Carla would like to be able to talk to experienced nerds in the<br>
+			community that share her same interests who can help make her dreams become reality and<br>
+			make some great friends in the process</p>
+
+
+		<h1>Likes:</h1>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<h1>Needs:</h1>
+		<ul>
+			<li>Corey needs to be notified when his answer/correction has been received</li>
+			<li> Corey also needs to be alerted if a response is made on the current thread </li>
+			<li> Corey also needs the ability to un-subscribe to a thread if needed</li>
+		</ul>
+		<h1>Frustrations:</h1>
+		<ul>
+			<li>He does not like having to be redirected to a log in page in order to leave comments, or answers<br>
+				to questions regarding different products offered </li>
+		</ul>
+		<h1>Technology</h1>
+		<h3>Comfort level :</h3>
+		<ul>
+			<li>Very comfortable with technology and gaming due to interest & hobby, but also occupation</li>
+			<li>HP - ENVY x360 2-in-1 13.3" Touch-Screen Laptop - AMD Ryzen 5 - 8GB Memory - 128GB Solid State Drive</li>
+			<li>Super Nintendo</li>
+			<li></li>
+			<li>Atari</li>
+			<li>Nintendo Wii</li>
+			<li>Home computer- Apple - 27" iMac® (Latest Model) - Intel Core i5 (3.4GHz) - 8GB Memory - 1TB Fusion Drive - Silver</li>
+			<li>iPhone X iOS 11: 2436x1125px</li>
+		</ul>
+	</body>
+</html>
