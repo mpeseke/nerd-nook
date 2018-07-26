62b8d2178545894767eafc539cd13e334f784b38
Marlon Peseke <mpeseke@gmail.com>
2018-07-26 09:06:38 -0600

    Added index page and link to Anthony Persona.

---
+++ epic/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			body {
+				font-family: sans-serif;
+			}
+		</style>
+		<title> Nerd Nook Directory Nav </title>
+
+	</head>
+
+	<body>
+		<h1>Nerd Nook Capstone Nav</h1>
+			<ul>
+				<li> <a href="anthony.php"> </a>Anthony McMillan</li>
+			</ul>
+
+	</body>
+</html>
