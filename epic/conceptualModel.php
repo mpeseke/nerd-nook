8e8d3a870c4ca2eb2f84b4d9117c7467a674c186
rbecker8 <rbecker8@cnm.edu>
2018-07-26 13:22:13 -0600

    Added profile entity & relatioships to conceptual model.

--- epic/conceptualModel.php
+++ epic/conceptualModel.php
@@ -4,7 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Conceptual Model</title>
 	</head>
-	<h3> Entities & Attributes</h3>
+	<h1> Entities & Attributes</h1>
 	<p>Profile</p>
 	<ul>
 		<li>profileId (Primary Key)</li>
@@ -13,7 +13,7 @@
 		<li>profileEmail</li>
 		<li>profileHash (For Account Password)</li>
 	</ul>
-	<p>Relationships</p>
+	<p>Relations</p>
 	<ul>
 		<li>One profile can attend multiple events (1 to n)</li>
 		<li>One profile can obtain multiple reputation points(1 to n)</li>
@@ -30,6 +30,8 @@
 	<h1> Relationships </h1>
 	<ul>
 	<li>many comments can be posted to one event ( 1 to n)</li>
-	<li>many comments can be posted by 1 profile (n to 1 )</li>
+	<li>many comments can be posted by 1 profile (m to 1 )</li>
 	</ul>
+	<a href="carlaUseCase.php">Use Case</a>
+	<a href="carla.php">Persona</a>
 </html>
