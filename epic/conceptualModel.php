7de1a8712d1bb0d3a19a9f4dcb5eddf433261691
rbecker8 <rbecker8@cnm.edu>
2018-07-26 13:28:53 -0600

    Moved profile relationships down to relationships section.

--- epic/conceptualModel.php
+++ epic/conceptualModel.php
@@ -4,6 +4,7 @@
 		<meta charset="UTF-8">
 		<title>Conceptual Model</title>
 	</head>
+	<body>
 	<h1> Entities & Attributes</h1>
 	<p>Profile</p>
 	<ul>
@@ -12,12 +13,6 @@
 		<li>profileAtHandle</li>
 		<li>profileEmail</li>
 		<li>profileHash (For Account Password)</li>
-	</ul>
-	<p>Relations</p>
-	<ul>
-		<li>One profile can attend multiple events (1 to n)</li>
-		<li>One profile can obtain multiple reputation points(1 to n)</li>
-		<li>One profile can comment multiple (1 to n)</li>
 	</ul>
 	<h3>Comments</h3>
 	<ul>
@@ -29,9 +24,13 @@
 	</ul>
 	<h1> Relationships </h1>
 	<ul>
+	<li>One profile can attend multiple events (1 to n)</li>
+	<li>One profile can obtain multiple reputation points(1 to n)</li>
+	<li>One profile can comment multiple (1 to n)</li>
 	<li>many comments can be posted to one event ( 1 to n)</li>
 	<li>many comments can be posted by 1 profile (m to 1 )</li>
 	</ul>
 	<a href="carlaUseCase.php">Use Case</a>
 	<a href="carla.php">Persona</a>
+	</body>
 </html>
