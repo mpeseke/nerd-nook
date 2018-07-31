2fa29ada498c93443359b78465429cc2a3f6a541
Marlon Peseke <mpeseke@gmail.com>
2018-07-31 15:24:53 -0600

    Removed JPG

--- epic/conceptualModel.php
+++ epic/conceptualModel.php
@@ -55,7 +55,6 @@
 		</ul>
 
 		<img src="../epic/content/capstone-erg.svg" alt="Nerd Nook ERD">
-		<img src="../epic/content/capstone-erg.jpeg" alt="Nerd Nook ERD JPG">
 
 		<a href="anthonyPersona.php">Persona- Anthony McMillan</a>
 		<a href="anthonyUseCase.php">Use Case- Anthony McMillan</a>
