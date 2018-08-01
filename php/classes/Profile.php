99e6f95048372d71d3a137765251f561c5e5070b
rbecker8 <rbecker8@cnm.edu>
2018-08-01 13:48:23 -0600

    Added attributes for the profile class.

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -10,3 +10,37 @@
  * @version 1.0
  **/
 
+class Profile {
+	use ValidateUuid;
+	/**
+	 * id for Profile; this is a primary key
+	 * @var Uuid $profileId
+	 **/
+	private $profileId;
+	/**
+	 * token handed out to verify that the profile is valid and not malicious
+	 * @var $profileActivationToken;
+	 **/
+	private $profileActivationToken;
+	/**
+	 * at handle for this Profile; this is a unique index
+	 * @var string $profileAtHandle
+	 *
+	 **/
+	private $profileAtHandle;
+	/**
+	 * email for this Profile; this is a unique index
+	 * @var string $profileEmail
+	 **/
+	private $profileEmail;
+	/**
+	 * hash for profile password
+	 * @var $profileHash
+	 **/
+
+
+
+
+
+
+}
