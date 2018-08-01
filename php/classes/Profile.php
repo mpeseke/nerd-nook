8eb2ba5903470b4a6480b40f4a2fc22b09430e57
rbecker8 <rbecker8@cnm.edu>
2018-08-01 16:21:08 -0600

    Added PDO statement to delete profile from mySQL. https://app.asana.com/0/inbox/762048584353828/765440304372175/765442323486478

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -252,8 +252,22 @@ class Profile {
 		$statement->execute($parameters);
 	}
 
+	/**
+	 * deletes this Profile from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo): void {
+		// create query template
+		$query = "DELETE FROM profile WHERE profileId = :profileId";
+		$statement = $pdo->prepare($query);
 
-
+		// bind the profile variables to the place holders in the template
+		$parameters = ["profileId" => $this->profileId->getBytes()];
+		$statement->execute($parameters);
+	}
 
 
 
