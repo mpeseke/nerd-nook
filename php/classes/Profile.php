1c6ef4ceb8d983415722cc20eaaccf0f46551856
rbecker8 <rbecker8@cnm.edu>
2018-08-01 13:41:34 -0600

    Created Profile php file under classes.  Added doc block for class with author and version.

---
+++ php/classes/Profile.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+ * Class Profile
+ *
+ * This data is representative of the stored data for a NerdNook Profile
+ *
+ * @author Ryan Becker <rbecker8@cnm.edu>
+ * @version 1.0
+ **/
+
