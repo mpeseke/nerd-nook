54e21ad44ec94a18d7d707bfaa8478a513e6ad27
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-02 17:03:54 -0600

    Worked on PDO statements on my Category class. https://app.asana.com/0/762061287654591/765440304372177 https://app.asana.com/0/762061287654591/765444547127606

--- php/classes/Category.php
+++ php/classes/Category.php
@@ -127,5 +127,88 @@ class Category {
 		//create query template
 		$query = "INSERT INTO category(categoryId, categoryName, categoryType) VALUES (:categoryId, :categoryName, :categoryType)";
 		$statement = $pdo->prepare($query);
+		$parameters = ["categoryId" => $this->categoryId->getBytes(), "categoryName" => $this->categoryName->getBytes(), "categoryType" => $this->categoryType->getBytes()];
+		$statement->execute($parameters);
 	}
+	/**
+	 * delete the category id frommySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function delete(\PDO $pdo): void {
+		//create query template
+		$query = "DELETE FROM category WHERE categoryId = :categoryId AND categoryName = :categoryName AND categoryType = :categoryType";
+		$statement = $pdo->prepare($query);
+		//bind the member variables to the place holders in the template
+		$parameters = ["categoryId" => $this->categoryId->getBytes()];
+		$statement->execute($parameters);
+	}
+	/**
+	 * gets the Category by category
+	 *
+	 *
+	 * @param \PDO $pdo PDO Connection object
+	 * @return Category|null Category or null if not found
+	 * @throws \PDOException when mySQL errors happen
+	 * @throws \TypeError when a variable is not the correct data type
+	 */
+	public static function getCategoryByCategoryIdAndCategoryNameAndCategoryType(\PDO $pdo, string $categoryId, string $categoryName, string $categoryType):?Category {
+		//sanitize the category id before searching
+		try {
+			$categoryId = self::ValidateUuid($categoryId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		try {
+			$categoryName = self::ValidateUuid($categoryName);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		try {
+			$categoryType = self::ValidateUuid($categoryType);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			throw (new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		//
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
