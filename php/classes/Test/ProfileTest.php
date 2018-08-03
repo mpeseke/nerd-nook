ab6904f58cb20895a6b5afa0f96172979ac541f4
rbecker8 <rbecker8@cnm.edu>
2018-08-02 22:05:21 -0600

    Added doc block and main class for ProfileTest.                                 https://app.asana.com/0/inbox/762048584353828/765440304372175/765442323486478

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -3,3 +3,30 @@
 namespace Rbecker8\NerdNook\Test;
 
 use Rbecker8\NerdNook\Profile;
+
+
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/autoload.php");
+
+// grab the uuid generator
+require_once(dirname(__DIR__, 2). "");
+
+/**
+ * Full PHPUnit test for the Profile class
+ *
+ * This is a complete PHPUnit test of the Profile class.  It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs
+ *
+ * @see Profile
+ * @author Ryan Becker <rbecker8@cnm.edu>
+ **/
+
+class ProfileTest extends DataDesignTest {
+	/**
+	 * placeholder until account activation is created
+	 * @var string $VALID_ACTIVATION
+	 *
+	 *
+	 */
+	protected $VALID_ACTIVATION;
