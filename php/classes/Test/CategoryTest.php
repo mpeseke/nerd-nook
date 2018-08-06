df50c9ac45def97764d66357622cbee0876622ee
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-06 15:56:08 -0600

    started work on the Category Test https://app.asana.com/0/762061287654591/765440304372177 https://app.asana.com/0/762061287654591/765444547127606

---
+++ php/classes/Test/CategoryTest.php
@@ -0,0 +1,8 @@
+<?php
+namespace CalebMHeckendorn;
+
+//this Grabs the class we want to look at
+require_once (dirname(__DIR__)) . "/autoload.php";
+
+//grabs the Uuid generator
+require_once (dirname(__DIR__, 2)) . "../lib/uuid.php";
