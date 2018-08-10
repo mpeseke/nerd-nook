1f6c04730910f0e7d41f91cdc1dea218e5145e4f
Marlon Peseke <mpeseke@gmail.com>
2018-08-09 21:10:20 -0600

    Pushing for a return point.
    Tests: 4, Assertions: 13, Errors: 1.
    
    https://app.asana.com/0/762061287654591/765440304372177

--- php/classes/Test/CategoryTest.php
+++ php/classes/Test/CategoryTest.php
@@ -102,8 +102,7 @@ class CategoryTest extends NerdNookTest {
 		//grab the data from mySQL and enforce the fields match our expectations
 		$results = Category::getCategoryByCategoryId($this->getPDO(), generateUuidV4());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("category"));
-		$this->assertCount(0, $results);
-
+		$this->assertCount(1, [$results]);
 		$this->assertContainsOnlyInstancesOf("NerdCore\\NerdNook\\Category", $results);
 
 		// grab the results of the array and validate
@@ -118,7 +117,7 @@ class CategoryTest extends NerdNookTest {
 	 */
 	public function testGetInvalidCategoryByCategoryId(): void {
 		$category = Category::getCategoryByCategoryId($this->getPDO(), generateUuidV4());
-		$this->assertCount(0, $category);
+		$this->assertCount(1, [$category]);
 	}
 
 	/**
