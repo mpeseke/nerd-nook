d7baf6543257f0b1788f44fde7ec15bd20aaded3
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-16 10:54:03 -0600

    C&P for review https://app.asana.com/0/762061287654591/780087369840080

--- public_html/api/checkIn/index.php
+++ public_html/api/checkIn/index.php
@@ -1,13 +1,14 @@
 <?php
+
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname("/etc/apache2/nerd-nook-mysql/encrypted-config.php");
 
 use NerdCore\NerdNook\{
-	Event,
-	Profile
+	CheckIn
 };
 
 /**
@@ -31,14 +32,86 @@ try {
 	$pdo = connectToEncryptedMySQL("/etc/apache2/nerd-nook-mysql/nerdnook.ini");
 
 	//determine what HTTP method was used
-	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	//sanitize parameters
 	$checkInProfileId = filter_input(INPUT_GET, "checkInProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$checkInEventId = filter_input(INPUT_GET, "checkInEventId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 //make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	if($method === "GET") {
+		//Set XRSF cookie
+		setXsrfCookie();
+
+		//gets a specific checkIn associated based on its composite key
+		if($checkInProfileId !== null && $checkInEventId !== null) {
+			$checkIn = CheckIn::getCheckInByCheckInEventIdAndCheckInProfileId($pdo, $checkInProfileId, $checkInEventId);
+
+			if($checkIn !== null) {
+				$reply->data = $checkIn;
+			}
+			//if none of the search parameters are met throw an exception
+		} else if(empty($checkInEventId) === false) {
+			$checkIn = CheckIn::getCheckInByCheckInEventId($pdo, $checkInEventId)->toArray();
+			if($checkIn !== null) {
+				$reply->data = $checkIn;
+			}
+			//get all the checkIns associated the the eventId
+		} else if(empty($checkInEventId) === false) {
+			$checkIn = CheckIn::getCheckInByCheckInProfileId($pdo, $checkInProfileId)->toArray();
+
+			if($checkIn !== null) {
+				$reply->data = $checkIn;
+			}
+		} else {
+			throw new InvalidArgumentException("Incorrect search parameters ", 404);
+		}
+
+	} else if($method === "POST" || $method === "PUT") {
+
+		//decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+		if(empty($requestObject->checkInEventId) === true) {
+			throw (new \InvalidArgumentException("No Event linked to the CheckIn", 405));
+		}
+
+		if(empty($requestObject->checkInProfileId) === true) {
+			throw (new \InvalidArgumentException("No Profile linked to the CheckIn", 405));
+		}
+
+		if(empty($requestObject->checkInDateTime) === true) {
+			$requestObject->checkInDateTime = date("y-m-d H:i:s");
+		}
+
+		if(empty($requestObject->checkInRep) === true) {
+			$requestObject->checkInRep = int("20");
+		}
+
+		if($method === "POST") {
+
+			//enforce that the end user has a XSRF token
+			verifyXsrf();
+		}
+
+			//enforce that the user is signed in
+			if(empty($_SESSION["profile"]) === true) {
+				throw(new \InvalidArgumentException("You must be logged in to Check In", 403));
+
+
+				$checkIn = new CheckIn($_SESSION["profile"]->getProfileId(), $requestObject->checkInEventId);
+				$checkIn->insert($pdo);
+				$reply->message = "Check In Successful!";
+			}
+			//if anyt other HTTP request is sent throw an exception
+		} else {
+			throw new \InvalidArgumentException("Invalid HTTP Request", 400);
+		}
+		//catch any exception that is thrown and update the status and message
+	} catch(\Exception | \TypeError $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
 	}
-}
+
+//encode and return reply to front end caller
+echo json_encode($reply);
