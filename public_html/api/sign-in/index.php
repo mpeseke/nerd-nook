8b978ee44a9d1c160cdc8d3c9532733160baf257
Chelsea David <cryan17@cnm.edu>
2018-08-16 09:31:30 -0600

    added empty reply https://app.asana.com/0/762048584353808/780087369840085

--- public_html/api/sign-in/index.php
+++ public_html/api/sign-in/index.php
@@ -4,3 +4,15 @@ require_once dirname(__DIR__,3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 use NerdCore\NerdNook\Profile;
+
+/**
+ *
+ * api for handling sign-in
+ *
+ * @author Cryan17
+ *
+ **/
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data=null;
