87dcd0c7680318a6eba8b8bde852c6b2599b3c10
rbecker8 <rbecker8@cnm.edu>
2018-08-16 16:50:16 -0600

    Edited path leading to profile class for api.

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -6,9 +6,8 @@ require_once(dirname(__DIR__, 3) . "/php/lib/jwt.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/uuid.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-use NerdCore\NerdNook\ {
-	Profile
-};
+
+use NerdCore\NerdNook\Profile;
 
 /**
  * API for Profile
