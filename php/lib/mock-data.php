0ca5dec7ea998926c14138217628debdac3cbb92
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-20 16:40:25 -0600

    Wrote the seed data https://app.asana.com/0/762061287654591/780087369840080 https://app.asana.com/0/762061287654591/780087369840081 https://app.asana.com/0/762061287654591/780087369840088

---
+++ php/lib/mock-data.php
@@ -0,0 +1,24 @@
+<?php
+use NerdCore\NerdNook\{Category};
+
+//grab the class we want to take a look at
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("uuid.php");
+
+//grab the uuid generator
+require_once("uuid.php");
+$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/nerdnook.ini");
+
+$category = new Category(generateUuidV4(), "Mariokart Wii", "Video Games");
+$category->insert($pdo);
+echo "first category";
+
+$category2 = new Category(generateUuidV4(), "Chess", "Board Games");
+$category->insert($pdo);
+echo "second category";
+
+$category3 = new Category(generateUuidV4(), "Harry Potter", "Books");
+$category->insert($pdo);
+echo "third category";
+
