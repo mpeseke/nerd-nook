0acbb5e94d7d4c8b9ca07d9000f49cd31ce9d23f
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-20 15:49:33 -0600

    Fixed the checkIn api index https://app.asana.com/0/762061287654591/780087369840080 https://app.asana.com/0/762061287654591/780087369840081 https://app.asana.com/0/762061287654591/780087369840088

--- public_html/api/checkIn/index.php
+++ public_html/api/checkIn/index.php
@@ -5,7 +5,9 @@ require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
-require_once dirname("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+
 
 use NerdCore\NerdNook\{
 	CheckIn
