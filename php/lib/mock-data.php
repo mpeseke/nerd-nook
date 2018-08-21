ca6146809d106d8ecf91f3ca777ec2c43e213fc2
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-21 09:59:29 -0600

    Got the mock data to  work https://app.asana.com/0/762061287654591/780087369840080 https://app.asana.com/0/762061287654591/780087369840081 https://app.asana.com/0/762061287654591/780087369840088

--- php/lib/mock-data.php
+++ php/lib/mock-data.php
@@ -2,7 +2,7 @@
 use NerdCore\NerdNook\{Category};
 
 
-//grab the class we want to take a look atutty
+//grab the class we want to take a look at
 
 require_once(dirname(__DIR__) . "/classes/autoload.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
@@ -19,10 +19,10 @@ echo "first category";
 
 
 $category2 = new Category(generateUuidV4(), "Dungeons and Dragons", "Table Games");
-$category->insert($pdo);
+$category2->insert($pdo);
 echo "second category";
 
 
 $category3 = new Category(generateUuidV4(), "Harry Potter", "Books");
-$category->insert($pdo);
+$category3->insert($pdo);
 echo "third category";
