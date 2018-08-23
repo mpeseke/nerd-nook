ae6d63c4d1362c61605d29095170cc355c10e204
Chelsea David <cryan17@cnm.edu>
2018-08-23 16:34:17 -0600

    added imports to vendor.ts

---
+++ src/vendor.ts
@@ -0,0 +1,20 @@
+// NerdNook
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
+
+// Other vendors for example Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import "jquery";
+import "bootstrap";
+
+// font awesome
+import "@fortawesome/fontawesome";
+import "@fortawesome/fontawesome-free-brands";
+import "@fortawesome/fontawesome-free-regular";
+import "@fortawesome/fontawesome-free-solid";
