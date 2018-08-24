90a18a6ea6da52d006b89933b14be33be60e3439
Chelsea David <cryan17@cnm.edu>
2018-08-24 10:20:33 -0600

    corrected import in routes to components/splash.component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import {RouterModule, Routes} from "@angular/router";
-// import {SplashComponent} from "./splash/splash.component";
+import {SplashComponent} from "./components/splash.component";
 import {UserService} from "./services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
 
