8b9988a95acf1f73eb843cb3aed1ac7ec8c522f5
Chelsea David <cryan17@cnm.edu>
2018-08-24 10:53:07 -0600

    added export components in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,34 @@
+// import needed @angularDependencies
 import {RouterModule, Routes} from "@angular/router";
+
+//import all needed Interceptors
 import {SplashComponent} from "./components/splash.component";
 import {UserService} from "./services/user.service";
 import {APP_BASE_HREF} from "@angular/common";
 
 
-export const allAppComponents = [SplashComponent];
+export const allAppComponents = [
+	SplashComponent,
+	EventAttendanceComponent,
+	CreateEventComponent,
+	EditEventComponent,
+	EditProfileComponent,
+	EditCommentComponent,
+	HomeComponent,
+	LandingPageComponent,
+	LoginNavComponent,
+	NavbarComponent,
+	EventComponent,
+	ProfileComponent,
+	CheckInComponent,
+	CommentComponent,
+	CategoryComponent,
+	SignInComponent,
+	SignUpComponent,
+	SignOutComponent,
+	SearchUsersComponent,
+	FileSelectDirective,
+];
 
 export const routes: Routes = [
 	{path: "", component: SplashComponent}
