c611a9d7a92df9adb8facb956b8e7de9418cbdb8
Chelsea David <cryan17@cnm.edu>
2018-08-25 15:54:16 -0600

    added imports for comment service

--- src/app/shared/services/auth.guard.service.ts
+++ src/app/shared/services/auth.guard.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import { Router, CanActivate} from "@angular/router";
-import {AuthService} from "../services/auth.service";
+import {AuthService} from "./auth.service";
 
 
 @Injectable()
--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -1,4 +1,14 @@
 import {Injectable} from "@angular/core";
 
 import{Status} from "../interfaces/status";
-import
+import {Comment} from "../interfaces/comment"
+import {Observable} from "rxjs/internal/Observable";
+import {HttpClient} from "@angular/common/http";
+
+@injectable ()
+export class CommentService {
+	constructor(protected http : HttpClient ) {
+
+		//define the API end point
+	}
+}
