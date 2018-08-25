0508facf6f52b223f674cf020b3a20c21f8cf818
Marlon Peseke <mpeseke@gmail.com>
2018-08-24 20:38:49 -0600

    Finished writing Deep Dive standard interceptor.

--- src/app/services/deep.dive.interceptor.ts
+++ src/app/services/deep.dive.interceptor.ts
@@ -1,6 +1,7 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 /**
  * class that intercepts data for Deep Dives' API standard
@@ -22,4 +23,46 @@ export class DeepDiveInterceptor implements HttpInterceptor {
 	 * @param {HttpHandler} next outgoing handler for next interceptor
 	 * @returns {Observable<HttpRequest<any>>} Observable for next interceptor to subscribe to
 	 */
+	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+		//hand off to next interceptor
+		return(next.handle(request).pipe(map((event:HttpEvent<any>) => {
+			//if this is an HTTP Response, from Angular...
+			if(event instanceof HttpResponse && event.body ==! null) {
+				//create an event to return (by default, return the same event)
+				let dataEvent = event;
+
+				//if the API is successful...
+				if(event.status === 200) {
+					//extract the JWT Header and put it in local storage
+					if(localStorage.getItem("jwt-token") === null) {
+						let jwtToken = event.headers.getAll("X-JWT-TOKEN");
+
+						if(jwtToken !== null) {
+							let token : string = jwtToken[0];
+							console.log(token);
+							localStorage.setItem("jwt-token", token.toString());
+						}
+					}
+					//extract the data or message from the response body
+					let body= event.body;
+					if(body.status === 200) {
+						if(body.data) {
+							//extract data returned from a GET request
+							dataEvent = event.clone({body: body.data});
+						} else if(body.message) {
+							//extract a successful message
+							dataEvent = event.clone({body: {message: body.message, status: 200, type: "alert-success"}});
+						}
+					} else {
+						//extract a failing message when the API fails
+						dataEvent = event.clone({body: {message: body.message, status: body.status, type: "alert-danger"}});
+					}
+				} else {
+					//extract a failing message when the web server fails
+					dataEvent = event.clone({body: {message: event.statusText, status: event.status, type: "alert-danger"}});
+				}
+				return(dataEvent);
+			}
+		})))
+	}
 }
