39792169cfe8fca3542d3e84c8f3a2e24186ccf1
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-08-26 08:42:29 -0600

    Added my checkIn.ts file

--- src/app/components/splash.component.ts
+++ src/app/components/splash.component.ts
@@ -1,4 +1,3 @@
-
 import {Component, OnInit} from "@angular/core";
 import {User} from "../shared/interfaces/user";
 import {UserService} from "../shared/services/user.service";
---
+++ src/app/shared/interfaces/checkIn.ts
@@ -0,0 +1,8 @@
+export interface CheckIn {
+
+	checkInEventId: number,
+	checkInProfileId: number,
+	checkInDateTime: any,
+	checkInRep: number,
+
+}
