37100bd1cc3631f9b9a9c92c183b1b44f14447b3
Chelsea David <cryan17@cnm.edu>
2018-08-25 21:12:41 -0600

    corrected get, create, anf edit in comment service

--- src/app/shared/services/comment.service.ts
+++ src/app/shared/services/comment.service.ts
@@ -4,32 +4,38 @@ import{Status} from "../interfaces/status";
 import {Comment} from "../interfaces/comment"
 import {Observable} from "rxjs/internal/Observable";
 import {HttpClient} from "@angular/common/http";
+import {Event} from "../interfaces/event";
 
 @injectable ()
 export class CommentService {
+
+	constructor(protected http : HttpClient ) {}
+
+
 		//define the API end point
 	private commentUrl = "api/comment/";
 
-	constructor(protected http : HttpClient ) {}
+
 
 	//call to the comment API and delete the comment in question
 	deleteComment(commentId: string) : Observable<Status> {
 		return(this.http.delete<Status>(this.commentUrl + commentId));
 	}
 
-	//call to the comment API anf create the comment in question
+//call to the comment API and edit the comment in question
 	editComment(comment : Comment) : Observable<Status> {
-		return(this.http.post<Status>(this.commentUrl. comment));
+		return(this.http.put<Status>(this.commentUrl + comment.commentId, comment));
+	}
+
+
+	// call to the comment API anf get a comment based on its ID
+	createComment(comment : Comment) : Observable<Status> {
+		return(this.http.post<Status>(this.commentUrl, event));
 	}
 
 	// call to the comment API anf get a comment based on its ID
-	createComment(commentId : string) : Observable<Comment> {
-		return(this.http.post<Comment>(this.commentUrl + commentId));
-	}
-
-	// call to the comment API anf get a comment based on its ID
-	getComment(commentId : string) : Observable<Comment> {
-		return(this.http.post<Comment>(this.commentUrl + commentId));
+	getComment(commentId : number) : Observable<Event> {
+		return(this.http.get<Event>(this.commentUrl + commentId));
 	}
 
 	// call to the API and get an array of comments based off the event id
