2467eead8605e5e70964828c57ca3c3d563bb2d5
rbecker8 <rbecker8@cnm.edu>
2018-08-27 13:27:58 -0600

    Added profile service type script file.

---
+++ src/app/shared/services/profile.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from "@angular/core";
+
+import {Profile} from "../interfaces/profile";
+import {Status} from "../interfaces/status";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable()
+export class ProfileService {
+
+	constructor(protected http: HttpClient) {
+
+	}
+
+	//define the API endpoint
+	private profileUrl = "api/profile/";
+
+	//reach out to the profile API and delete the profile in question
+	deleteProfile(id : string) : Observable<Status> {
+		return(this.http.delete<Status>(this.profileUrl + id));
+	}
+
+	//call to the profile API and edit the profile in question
+	editProfile(profile: Profile) : Observable<Status> {
+		return(this.http.put<Status>(this.profileUrl , profile));
+	}
+
+	//call to the profile API and get Profile object by its id
+	getProfile(id: string) : Observable<Profile> {
+		return(this.http.get<Profile>(this.profileUrl + id));
+	}
+
+	//call to the profile API to grab an array of profiles based on user input
+	getProfileByProfileAtHandle(profileAtHandle: string) :Observable<Profile[]> {
+		return(this.http.get<Profile[]>(this.profileUrl + "?profileAtHandle=" + profileAtHandle));
+	}
+
+	//call to the profile API to grab corresponding profile by its email
+	getProfileByProfileEmail(profileEmail: string) :Observable<Profile[]> {
+		return(this.http.get<Profile[]>(this.profileUrl + "?profileEmail=" + profileEmail));
+	}
+}
