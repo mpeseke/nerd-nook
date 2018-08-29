38a09d16fdaf61cabdd8920fb85a5091078a1a5d
rbecker8 <rbecker8@cnm.edu>
2018-08-29 15:40:03 -0600

    Added static ui file for profile.  Added basic html structure, head with meta tags and nav bar.

---
+++ static-ui/profile.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
+			<meta name="author" content="Caleb, Chelsea, Marlon, Ryan"/>
+			<meta name="description" content="NerdNook, is a social platform allowing people to search and attend user created events for various hobbies & passions."/>
+
+			<!-- Bootstrap CSS -->
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+			<!-- Font Awesome link -->
+			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+			<!-- NerdNook CSS -->
+			<link rel="stylesheet" href="./styles.css">
+
+			<!-- jQuery, Popper.js, Bootstrap JS -->
+			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		</head>
+	<body>
+				<!-- start of page, header/navbar -->
+				<nav class="nav nav-pills nav-fill sticky-top navbar-dark bg-dark">
+					<a class="nav-item nav-link" href="#">Home</a>
+					<a class="nav-item nav-link" href="#">Categories</a>
+					<a class="nav-item nav-link" href="#">Events</a>
+					<a class="nav-item nav-link" href="#">Sign Out</a>
+				</nav>
+
+
+
+
+
+	</body>
+</html>
