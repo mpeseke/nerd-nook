f587058498ea73bd1a07627cf41476bafc70958f
Marlon Peseke <mpeseke@gmail.com>
2018-08-29 21:37:32 -0600

    corrected bootstrap nav to collapse.

---
+++ static-ui/event.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<!-- jQuery for Bootstrap -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<!-- CSS Custom -->
+		<style>
+			.jumbotron {
+				background: linear-gradient(rgba(0, 0, 0, 0.15) 0%, rgba(0, 0, 0, 0.65) 100%), url("content/chess.jpg") no-repeat;
+				img {
+					width: 100%;
+				}
+			}
+			.navbar-nav {
+				margin: 0 auto }
+			.nav-item {
+				display: flex;
+				justify-content: center;
+				margin-right: 5em;
+				margin-left: 5em;
+			}
+		</style>
+
+
+		<title>The Nerd Nook Events</title>
+	</head>
+	<body>
+		<header>
+			<nav class="nav navbar-expand-md sticky-top navbar-dark bg-dark p-2">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="nerdNav">
+					<span class="navbar-toggler-icon"><i class="fas fa-glasses"></i></span>
+				</button>
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-home pr-2"></i>Home</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-journal-whills fa-fw pr-2"></i>Categories</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-calendar-check fa-fw pr-2"></i>Events</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-sign-out-alt fa-fw pr-2"></i>Sign Out</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+
+		<div class="jumbotron jumbotron-fluid text-light">
+			<div class="container">
+				<h1 class="display-4">Events</h1>
+				<p class="lead"><strong>Nerd Herd</strong>, <em>noun</em>,<br/>
+				A large group of nerd of either one or multiple kinds, who are mutually friends.</p>
+			</div>
+
+		</div>
+
+
+	</body>
+</html>
