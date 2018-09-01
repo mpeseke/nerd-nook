ebf2b015f730a79317bef77739f830dd8ddb4b03
Marlon Peseke <mpeseke@gmail.com>
2018-09-01 12:03:58 -0600

    Added static UI for the nerdnook navbar... committing and pushing for use for the team!

---
+++ static-ui/content/navbar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
+
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<!-- jQuery for Bootstrap -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+		<!-- CSS Custom -->
+		<style>
+
+		</style>
+
+
+		<title>The Nerd Nook Events</title>
+	</head>
+
+	<!--NerdNook Standard NavBar -->
+
+	<header>
+		<nav class="navbar navbar-expand-md navbar-dark bg-dark p-2">
+			<a class="navbar-brand" href="#">The Nerd Nook</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nerdNav">
+				<span><i class="fas fa-glasses"></i></span>
+			</button>
+			<div class="collapse navbar-collapse" id="nerdNav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-home pr-2"></i>Home</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-journal-whills fa-fw pr-2"></i>Categories</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-calendar-check fa-fw pr-2"></i>Events</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link text-light" href="#"><i class="fas fa-sign-out-alt fa-fw pr-2"></i>Sign Out</a>
+					</li>
+				</ul>
+			</div>
+
+		</nav>
+	</header>
+
+</html>
