0a4437ca32f33dd08983cf5880f78e7a95239348
rbecker8 <rbecker8@cnm.edu>
2018-09-03 21:35:06 -0600

    Corrected sticky footer, as well as general font using Marlon's meta links.

--- static-ui/profile.php
+++ static-ui/profile.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
-<html lang="en-US">
-		<head>
-			<meta charset="utf-8"/>
-			<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width = device-width, initial-scale = 1, shrink-to-fit = no"/>
 
-			<!-- Bootstrap CSS -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<!-- Bootstrap -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<!-- jQuery for Bootstrap -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-			<!-- jQuery, Popper.js, Bootstrap JS -->
-			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-			<!-- Font Awesome link -->
-			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
 			<!-- CSS Custom -->
 			<style>
@@ -25,17 +24,25 @@
 				}
 
 				.footer {
-					position: sticky;
-					bottom: 0;
-					clear: both;
 					background-color: #53A451;
+					position: fixed;
+					left: 0;
+					bottom: 0;
+					width: 100%;
 					color: white;
+					text-align: center;
+					padding: 5px;
+					margin-top: 10px;
 				}
 
 				span:after{
 					content:" ";
-					display:inline-block;
+					display: inline-block;
 					width:32px;
+				}
+
+				span {
+					font-size: x-large;
 				}
 			</style>
 
@@ -79,27 +86,23 @@
 		<section>
 				<div class="row mx-0">
 					<div class="col-md-5 px-4">
-							<h1 class="profileName">First Name Last Name</h1>
-							<p class="profileDetails">
-							@Handle <br>
-							Join Date
-							</p>
+							<h2 class="profileFirstName">First Name</h2>
+							<h2 class="profileLastName">Last Name</h2>
+							<hr/>
 					</div>
 
 					<div class="col-md-7">
-						<h4 class="profileAtHandle">@handle:</h4><p>"   "</p>
-						<h4 class="joinDate">Join Date:</h4><p>"   "</p>
+						<h2 class="profileAtHandle">@handle</h2>
+						<h2 class="joinDate">Join Date</h2>
 						<hr/>
 					</div>
 				</div>
 		</section>
 
-				<footer>
-					<div class="footer container-fluid text-center text-monospace mt-5 pt-4 pb-3">
-						<span>Rep:</span>
-						<span>Comments:</span>
-						<span>Events:</span>
-					</div>
-				</footer>
+		<div class="footer container-fluid text-center text-monospace mt-5 pt-4 pb-3">
+			<span>Rep:</span>
+			<span>Comments:</span>
+			<span>Events:</span>
+		</div>
 	</body>
 </html>
