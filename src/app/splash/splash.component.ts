681ce57b133628b5cc19c04b9a11d4ec4c9b1bba
mpeseke <mpeseke@gmail.com>
2018-09-05 14:25:47 -0600

    Keeping time with the ts ts ts ts. 1 and 2 and 3 and...

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,14 +1,61 @@
 import {Component, OnInit} from "@angular/core";
-import {User} from "../shared/interfaces/user";
+import {Observable} from "rxjs";
+import {Router} from "@angular/router";
+import {Status} from "../shared/interfaces/status";
+import {SignUp} from "../shared/interfaces/sign.up";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {SignUpService} from "../shared/services/sign.up.service";
+import {SignIn} from "../shared/interfaces/sign.in";
+
+
+//declare $ for use of jQuery
+declare let $: any;
+
+//set the template url and the selector for the ng powered html tag
 
 @Component({
-	template: require("./splash.template.html")
+	template: require
+	("./splash.template.html"),
+	selector: "sign-up"
 })
 
 export class SplashComponent implements OnInit {
 
-	constructor() {}
+	signUpForm: FormGroup;
+	status: Status = {status: null, message: null, type: null};
+
+	constructor(private formBuilder: FormBuilder, private router: Router, private signUpService: SignUpService) {
+	}
 
 	ngOnInit(): void {
+		this.signUpForm = this.formBuilder.group({
+			atHandle: ["", [Validators.maxLength(32), Validators.required]],
+			email: ["", [Validators.maxLength(128), Validators.required, Validators.email]],
+			password: ["", [Validators.minLength(8), Validators.maxLength(48), Validators.required]],
+			passwordConfirm: ["", [Validators.minLength(8), Validators.maxLength(48), Validators.required]]
+		});
+
+		this.status = {status: null, message: null, type: null}
+
+
+	}
+
+	createSignUp(): void {
+
+		let signUp: SignUp = {
+			profileAtHandle: this.signUpForm.value.atHandle,
+			profileEmail: this.signUpForm.value.email,
+			profilePassword: this.signUpForm.value.password,
+			profilePasswordConfirm: this.signUpForm.value.passwordConfirm
+		};
+
+		this.signUpService.createProfile(signUp)
+			.subscribe(status => {
+				this.status = status;
+
+				if(this.status.status === 200) {
+					this.router.navigate([""])
+				}
+			});
 	}
 }
