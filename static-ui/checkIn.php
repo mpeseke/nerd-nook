04876bd34c4445b3b24cb3871d2724b5c9989e2b
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-09-05 14:53:27 -0600

    finished the checkIn static ui

--- static-ui/checkIn.php
+++ static-ui/checkIn.php
@@ -2,44 +2,45 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+				integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+				crossorigin="anonymous"/>
 		<!-- Bootstrap -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+				integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<!-- jQuery for Bootstrap -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+				  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+				  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+				  crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+				  integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+				  crossorigin="anonymous"></script>
 
-		<!-- Custom CSS -->
-		<style>
-			body {
-				background: black;
-				margin: 0;
-			}
-
-			.centered {
-				position: fixed;
-				top: 50%;
-				left: 50%;
-				/* bring your own prefixes */
-				transform: translate(-50%, -50%);
-			}
-
-
-		</style>
-
-		<title>Welcome...</title>
+		<title></title>
 	</head>
 	<body>
-		<main class="container-fluid">
-			<div class="row m-0 justify-content-center">
-				<div class="card centered bg-dark col-md-4 p-4 text-success text-monospace element">
-					<h5 class="card-title">Congratulations</h5>
-
-					<p class="card-text">Your account has been successfully verified. Thank you for signing up. Click the button to continue.</p>
-					<a href="#" class="btn btn-success">Assimilate</a>
+		<div class="row mx-0 justify-content-center text-center centered">
+			<div class="card text-success bg-dark col-lg-4 col-md-6 col-sm-6 m-2">
+				<div class="card-body">
+					<p class="card-text fsz">Thank you for RSVP'ing</p>
+					<i class="fas fa-calendar-check ico"></i>
+					<p class="card-text fsz">We look forward to seeing you there! </p>
 				</div>
 			</div>
-		</main>
+		</div>
+
+		<div class="row mx-0 justify-content-center text-center centered">
+			<div class="card text-success bg-dark col-lg-4 col-md-6 col-sm-6 m-2">
+				<div class="card-body">
+					<p class="card-text fsz">Check In</p>
+					<i class="fas fa-check-circle ico"></i>
+					<p class="card-text fsz">Successful</p>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>
