45b1770de4c14d0328874a9033cad1a5550b309b
mpeseke <mpeseke@gmail.com>
2018-09-05 14:57:44 -0600

    Added services for forms.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
@@ -7,7 +8,7 @@ import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, routing],
+	imports:      [BrowserModule, HttpClientModule, routing, FormsModule, ReactiveFormsModule],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
