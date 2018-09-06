6f4272363a38eef93c3c5eb8d1d983c10d39bf73
Chelsea David <cryan17@cnm.edu>
2018-09-06 14:10:02 -0600

    changed img directory path for landing page component

---
+++ static-ui/landingpage.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<style>
+			.card-img-top {
+				padding-top: 1rem;
+				width: 20em;
+				height: 275px;
+			}
+		</style>
+	</head>
+	<body>
+
+		<div class="row mx-0 justify-content-center">
+			<div class="card text-success bg-dark col-lg-4 col-md-6 col-sm-10 m-2">
+				<div class="card">
+					<img class="card-img-top rounded-circle" src="/src/app/landing-page/harold-profile.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Profile</h4>
+						<p class="card-text">Nerdalize your profile</p>
+						<a href="#" class="btn btn-dark">Go to Profile</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row mx-0 justify-content-center">
+			<div class="card text-success bg-dark col-lg-4 col-md-6 col-sm-10 m-2">
+				<div class="card">
+					<img class="card-img-top" src="/src/app/landing-page/category-landing-page.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Categories</h4>
+						<a href="#" class="btn btn-dark">Select a Category</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row mx-0 justify-content-center">
+			<div class="card text-success bg-dark col-lg-4 col-md-6 col-sm-10 m-2">
+				<div class="card">
+					<img class="card-img-top" src="/src/app/landing-page/events-card.jpg" alt="Card image cap">
+					<div class="card-body">
+						<h4 class="card-title">Events</h4>
+						<a href="#" class="btn btn-dark">Create/View Events</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
