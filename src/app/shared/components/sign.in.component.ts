3328faf8eb3ad3043bad7d604c478842a780a2f5
mpeseke <mpeseke@gmail.com>
2018-09-06 10:04:57 -0600

    Finished shared sign in component

--- src/app/shared/components/sign.in.component.ts
+++ src/app/shared/components/sign.in.component.ts
@@ -1,11 +1,11 @@
 import {Component, ViewChild} from "@angular/core";
 
+import {Router} from "@angular/router";
 import {Status} from "../interfaces/status";
 import {SignIn} from "../interfaces/sign.in";
 import {SignInService} from "../services/sign.in.service";
 import {CookieService} from "ng2-cookies";
-
-
+declare var $: any;
 
 
 @Component ({
@@ -24,4 +24,20 @@ export class SignInComponent {
 
 
 
+	signIn(): void {
+		localStorage.removeItem("jwt-token");
+		this.SignInService.postSignIn(this.signin).subscribe(status => {
+			this.status = status;
+
+			if(status.status === 200) {
+				this.router.navigate(["profile-page"]); //this will need to be changed to our landing page
+				this.signInForm.reset();
+				setTimeout(1000, function() {
+					$("signin-modal").modal('hide');
+				});
+			} else {
+				console.log("failed login")
+			}
+		});
+	}
 }
