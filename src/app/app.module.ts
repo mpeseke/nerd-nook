67f2a1a3b98ad9f3fe9edb3be3a4e34d82428833
rbecker8 <rbecker8@cnm.edu>
2018-09-07 13:21:37 -0600

    Added JWT module to app.module.ts.  Also changed component template for profile.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,27 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
+import {JwtModule} from "@auth0/angular-jwt";
 
 const moduleDeclarations = [AppComponent];
 
+//configure the parameters for the JwtModule
+const JwtHelper = JwtModule.forRoot({
+		config: {
+					tokenGetter: () => {
+								return localStorage.getItem("jwt-token");
+					},
+					skipWhenExpired:true,
+					whitelistedDomains: ["localhost:7272", "https://bootcamp-coders.cnm.edu/"],
+					headerName:"X-JWT-TOKEN",
+					authScheme: ""
+		}
+});
+
+
+
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, routing, FormsModule, ReactiveFormsModule],
+	imports:      [BrowserModule, HttpClientModule, JwtHelper, routing, FormsModule, ReactiveFormsModule],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,24 +5,29 @@ import {JwtHelperService} from "@auth0/angular-jwt";
 import {ProfileService} from "../shared/services/profile.service";
 
 @Component({
-		template: require("./profile.component.html")
+	template: `
+		<h1>{{profile.profileAtHandle}}</h1>
+
+	`
 })
 
-export class ProfileComponent {
-	// profile: Profile;
-	// status: Status;
-	//
-	// constructor(private profileService: ProfileService, private jwtHelper : JwtHelperService) {}
-	//
-	// ngOnInit(): void {
-	// 	this.currentlySignedIn()
-	// }
-	//
-	// currentlySignedIn() : void {
-	//
-	// 	const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
-	//
-	// 	this.profileService.getProfile(decodedJwt.auth.profileId)
-	// 		.subscribe(profile => this.profile = profile)
-	// }
+	export class ProfileComponent implements OnInit{
+
+	profile: Profile;
+	status: Status;
+
+	constructor(private profileService: ProfileService, private jwtHelper : JwtHelperService) {}
+
+	ngOnInit(): void {
+		this.currentlySignedIn()
+	}
+
+	currentlySignedIn() : void {
+
+		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
+
+		this.profileService.getProfile(decodedJwt.auth.profileId)
+			.subscribe(profile => this.profile = profile)
+
+	}
 }
