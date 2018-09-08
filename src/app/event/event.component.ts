708e13e36d2743119754aab3ea74642176aceeaa
Chelsea David <cryan17@cnm.edu>
2018-09-08 17:06:02 -0600

    added category service to event component

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {Profile} from "../shared/interfaces/profile";
 import {ProfileComponent} from "../profile/profile.component";
 import {EventService} from "../shared/services/event.service";
-import {CategoryService} from "../shared/services/category.service";
+// import {CategoryComponent} from "../category/category.component";
 
 
 @Component ({
