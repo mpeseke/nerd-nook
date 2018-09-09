9b27fd2d89508562d7b3cf234f78b9f55848f3e2
mpeseke <mpeseke@gmail.com>
2018-09-09 17:13:20 -0600

    Changed check in and RSVP buttons back to original styling. Will work with Caleb on the pop-ups

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,47 +1,13 @@
 import {Component, OnInit} from "@angular/core";
-import {Event} from "../shared/interfaces/event";
-import {ActivatedRoute} from "@angular/router";
 import {Profile} from "../shared/interfaces/profile";
-import {Status} from "../shared/interfaces/status";
+import {ProfileComponent} from "../profile/profile.component";
 import {EventService} from "../shared/services/event.service";
-import {CheckInService} from "../shared/services/checkIn.service";
-import {ProfileService} from "../shared/services/profile.service";
-import {CategoryComponent} from "../category/category.component";
+// import {CategoryComponent} from "../category/category.component";
 
-@Component({
-	template: require("./event.component.html"),
-	selector: "event"
+
+@Component ({
+	template: require("./event.component.html")
 })
+export class EventComponent  {
 
-export class EventComponent implements OnInit {
-
-	event: Event;
-	profile: Profile;
-	eventId = this.route.snapshot.params["eventId"];
-	status: Status;
-
-	constructor(protected eventService: EventService, protected checkInService: CheckInService, protected route: ActivatedRoute, private profileService: ProfileService){
-
-	}
-
-	ngOnInit(): void {
-		this.currentlySignedIn()
-	}
-
-	currentlySignedIn(): void {
-		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
-		this.profileService.getProfile(decodedJwt.auth.profileId).subscribe(profile => this.profile = profile)
-	}
-
-	rsvp() {
-		this.checkInService.createCheckIn(this.eventId).subscribe(status => {
-			this.status = status;
-		});
-	}
-
-	checkIntoEvent() {
-		this.checkInService.editCheckIn(this.checkInEventId).subscribe(status => {
-			this.status = status;
-		});
-	}
 }
