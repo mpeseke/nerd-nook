610ee1befee6c07ea2fbea4ea3d34ceb5bca2d88
rbecker8 <rbecker8@cnm.edu>
2018-09-11 10:00:57 -0600

    Used interpolation to show profile email and at handle.  Also corrected path for profile.ts file.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -31,10 +31,6 @@ import {JwtHelperService} from "@auth0/angular-jwt";
 	currentlySignedIn() : void {
 
 		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
-
-		this.profileService.getProfileByProfileAtHandle(decodedJwt.auth.profileId)
-			.subscribe(profile => this.profile = profile)
-
 	}
 
 	getProfile() {
