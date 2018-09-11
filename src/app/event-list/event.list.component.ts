fce3cc19e12e35b59baccef66071fba16fde6321
mpeseke <mpeseke@gmail.com>
2018-09-11 10:31:05 -0600

    Changes to event list components. Working next on stringing data from the creation of an event to the actual event list.

--- src/app/event-list/event.list.component.ts
+++ src/app/event-list/event.list.component.ts
@@ -1,11 +1,29 @@
 import {Component, OnInit} from "@angular/core";
-
+import {EventService} from "../shared/services/event.service";
+import {Status} from "../shared/interfaces/status";
+import { Event } from "../shared/interfaces/event";
+import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Component ({
 	template: require("./event.list.component.html")
 })
 
-export class EventListComponent  {
+export class EventListComponent implements OnInit{
+	event: Event;
 
+	constructor (protected eventService: EventService, protected  router: ActivatedRoute) {
+
+	}
+	eventId = this.router.snapshot.params["eventId"];
+	ngOnInit() {
+		window.sessionStorage.setItem('url', window.location.href);
+		this.loadEvent();
+	}
+
+	loadEvent() {
+		this.eventService.getEvent(this.eventId).subscribe(reply => {
+			this.event = reply;
+		});
+	}
 }
