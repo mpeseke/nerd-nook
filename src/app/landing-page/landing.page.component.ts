995c8992a35d86264fc2a0e1b66b9808f06039cc
rbecker8 <rbecker8@cnm.edu>
2018-09-11 13:49:08 -0600

    Changed header routing to include profile at handle.

--- src/app/landing-page/landing.page.component.ts
+++ src/app/landing-page/landing.page.component.ts
@@ -1,7 +1,42 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ProfileService} from "../shared/services/profile.service";
+import {Profile} from "../shared/interfaces/profile";
+import {ActivatedRoute} from "@angular/router";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component ({
 	template: require("./landing.page.component.html")
 })
 
-export class LandingPageComponent {}
+export class LandingPageComponent implements OnInit{
+
+	profile : Profile =  {
+		profileActivationToken: null,
+		profileAtHandle: null,
+		profileEmail: null,
+		profileHash: null,
+		profileId: null,
+};
+
+
+profileId : string = this.route.snapshot.params["id"];
+
+	constructor(private profileService: ProfileService, private route: ActivatedRoute, private jwtHelper : JwtHelperService) {}
+
+	ngOnInit(): void {
+	this.currentlySignedIn();
+	this.getProfile()
+	}
+
+	currentlySignedIn() : void {
+
+	const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
+}
+
+	getProfile() {
+		this.profileService.getProfileByProfileId(this.profileId)
+			.subscribe(profile => this.profile = profile);
+
+	}
+
+}
