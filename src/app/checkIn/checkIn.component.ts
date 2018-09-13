9018f2a098857950bd492809f9f89686336bcacc
Caleb M. Heckendorn <mrchicken@jcangus.com>
2018-09-13 10:07:08 -0600

    Added checkIn to Event typescript

--- src/app/checkIn/checkIn.component.ts
+++ src/app/checkIn/checkIn.component.ts
@@ -1,65 +1,62 @@
-import {Component, OnInit} from "@angular/core";
-import {CheckIn} from "../shared/interfaces/checkIn";
-import {ActivatedRoute} from "@angular/router";
-import {Event} from "../shared/interfaces/event";
-import {Profile} from "../shared/interfaces/profile";
-import {Status} from "../shared/interfaces/status";
-import {CheckInService} from "../shared/services/checkIn.service";
-import {JwtHelperService} from "@auth0/angular-jwt";
-import {ProfileService} from "../shared/services/profile.service";
-
-
-//Declare $ for use of jQuery
-// declare let $: any;
-
-@Component({
-	template: require("./checkIn.component.html"),
-	selector: "checkIn"
-})
-
-export class CheckInComponent implements OnInit {
-
-	checkIn: CheckIn;
-	event: Event;
-	checkInEventId = this.route.snapshot.params["checkInEventId"];
-
-
-	profile: Profile;
-	status: Status;
-
-	constructor(protected checkInService: CheckInService, protected route: ActivatedRoute, private profileService: ProfileService, private jwtHelper: JwtHelperService) {
-
-	}
-
-	ngOnInit(): void {
-		this.currentlySignedIn()
-	}
-
-	currentlySignedIn(): void {
-		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
-		this.profileService.getProfile(decodedJwt.auth.profileId).subscribe(profile => this.profile = profile)
-	}
-
-	rsvp(): void {
-
-		this.checkInService.createCheckIn(this.checkInEventId)
-			.subscribe(status => {
-				this.status = status;
-
-				if(this.status.status === 200) {
-					alert(status.message);
-				}
-			});
-	}
-
-	checkIntoEvent(): void {
-		this.checkInService.editCheckIn(this.checkInEventId)
-			.subscribe(status => {
-				this.status = status;
-
-				if(this.status.status === 200) {
-					alert(status.message);
-				}
-		});
-	}
-}
+// import {Component, OnInit} from "@angular/core";
+// import {CheckIn} from "../shared/interfaces/checkIn";
+// import {ActivatedRoute} from "@angular/router";
+// import {Event} from "../shared/interfaces/event";
+// import {Profile} from "../shared/interfaces/profile";
+// import {Status} from "../shared/interfaces/status";
+// import {CheckInService} from "../shared/services/checkIn.service";
+// import {JwtHelperService} from "@auth0/angular-jwt";
+// import {ProfileService} from "../shared/services/profile.service";
+// import {EventService} from "../shared/services/event.service";
+//
+//
+// //Declare $ for use of jQuery
+// // declare let $: any;
+//
+// @Component({
+// 	template: require("./checkIn.component.html"),
+// 	selector: "checkIn"
+// })
+//
+// export class CheckInComponent implements OnInit {
+//
+// 	checkIn: CheckIn;
+// 	event: Event = {
+// 		eventId: null,
+// 		eventCategoryId: null,
+// 		eventProfileId: null,
+// 		eventDetails: null,
+// 		eventEndDateTime: null,
+// 		eventLat: null,
+// 		eventLong: null,
+// 		eventName:null,
+// 		eventStartDateTime: null
+// 	};
+// 	eventId = this.route.snapshot.params["eventId"];
+// 	profile: Profile;
+// 	status: Status;
+//
+//
+// 	constructor(protected checkInService: CheckInService, protected eventService: EventService, protected route: ActivatedRoute, private profileService: ProfileService, private jwtHelper: JwtHelperService) {
+//
+// 	}
+//
+// 	ngOnInit(): void {
+// 		this.currentlySignedIn();
+// 	}
+//
+//
+//
+//
+//
+// 	checkIntoEvent(): void {
+// 		this.checkInService.editCheckIn(this.eventId)
+// 			.subscribe(status => {
+// 				this.status = status;
+//
+// 				if(this.status.status === 200) {
+// 					alert(status.message);
+// 				}
+// 		});
+// 	}
+// }
--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -2,40 +2,53 @@ import {Component, OnInit} from "@angular/core";
 import {Event} from "../shared/interfaces/event";
 import {ActivatedRoute} from "@angular/router";
 import {Profile} from "../shared/interfaces/profile";
+import {CheckIn} from "../shared/interfaces/checkIn";
 import {Status} from "../shared/interfaces/status";
 import {EventService} from "../shared/services/event.service";
 import {AuthService} from "../shared/services/auth.service";
-import {importType} from "@angular/compiler/src/output/output_ast";
-import {AddEventComponent} from "../add-event/add.event.component";
+import {CheckInService} from "../shared/services/checkIn.service";
+import {ProfileService} from "../shared/services/profile.service";
+import {JwtHelperService} from "@auth0/angular-jwt";
 
 @Component({
 	template: require("./event.component.html"),
 	selector: "event"
 })
-
 export class EventComponent implements OnInit {
-
-	event: Event = {eventId: null, eventCategoryId: null, eventProfileId: null, eventDetails: null,
-		eventEndDateTime: null, eventLat: null, eventLong: null, eventName:null, eventStartDateTime: null};
-	profile: Profile = {profileId: null, profileActivationToken: null, profileAtHandle: null, profileEmail: null, profileHash: null};
+	event: Event = {
+		eventId: null,
+		eventCategoryId: null,
+		eventProfileId: null,
+		eventDetails: null,
+		eventEndDateTime: null,
+		eventLat: null,
+		eventLong: null,
+		eventName: null,
+		eventStartDateTime: null
+	};
+	profile: Profile = {
+		profileId: null,
+		profileActivationToken: null,
+		profileAtHandle: null,
+		profileEmail: null,
+		profileHash: null
+	};
 	status: Status;
-
-	constructor(protected eventService: EventService, protected route: ActivatedRoute, private authService: AuthService){
-
+	checkIn: CheckIn;
+	constructor(protected eventService: EventService, protected checkInService: CheckInService, private profileService: ProfileService, private jwtHelper: JwtHelperService, protected route: ActivatedRoute, private authService: AuthService) {
 	}
 	eventId = this.route.snapshot.params["eventId"];
 	ngOnInit() {
 		window.sessionStorage.setItem('url', window.location.pathname);
 		this.loadEvent();
+		this.currentlySignedIn();
 		this.profile = this.getJwtProfileId();
 	}
-
 	loadEvent() {
 		this.eventService.getEvent(this.eventId).subscribe(reply => {
 			this.event = reply;
 		});
 	}
-
 	getJwtProfileId(): any {
 		if(this.authService.loggedIn()) {
 			return this.authService.decodeJwt().auth.profileId;
@@ -43,7 +56,26 @@ export class EventComponent implements OnInit {
 			return false;
 		}
 	}
-
-
-
+	currentlySignedIn(): void {
+		const decodedJwt = this.jwtHelper.decodeToken(localStorage.getItem('jwt-token'));
+		this.profileService.getProfile(decodedJwt.auth.profileId).subscribe(profile => this.profile = profile)
+	}
+	rsvp(): void {
+		this.checkInService.createCheckIn(this.eventId)
+			.subscribe(status => {
+				this.status = status;
+				if(this.status.status === 200) {
+					alert(status.message);
+				}
+			});
+	}
+	checkIntoEvent(): void {
+		this.checkInService.editCheckIn(this.eventId)
+			.subscribe(status => {
+				this.status = status;
+				if(this.status.status === 200) {
+					alert(status.message);
+				}
+			});
+	}
 }
